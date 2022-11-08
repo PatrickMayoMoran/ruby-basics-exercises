b6b6582d98559bdeb0935fd394e439eaac9f466f
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-11-08 15:50:26 +0000

    add array indexing problem

---
+++ debugging/colorful_things.rb
@@ -0,0 +1,23 @@
+# The problem was that we were looking beyond the length of the array
+# because indexing starts at 0. So we were getting nil values returned
+# This can be fixed by using the index of the shorter array
+# If we're not sure which array is shorter first, we could write
+# a function to return the array with the smaller length
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+colors.shuffle!
+things.shuffle!
+
+i = 0
+loop do
+  break if i > things.length - 1
+
+  if i == 0
+    puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+  else
+    puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+  end
+
+  i += 1
+end
