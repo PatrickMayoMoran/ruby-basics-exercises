0c424f9a8f949880f9408769d6cc32394118baba
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-11-08 12:33:42 +0000

    add multiply example

---
+++ debugging/multiple_by_five.rb
@@ -0,0 +1,11 @@
+def multiply_by_five(n)
+  n * 5
+end
+
+puts "Hello! Which number would you like to multiply by 5?"
+number = gets.chomp
+
+puts "The result is #{multiply_by_five(number)}!"
+
+# The user input is a string and needs to be converted to an integer
+# to get the result desired
