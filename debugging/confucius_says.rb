4fd5feb59e3db3de663416caf0236ed899526f72
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-11-08 15:36:55 +0000

    add explicit return exercise

---
+++ debugging/confucius_says.rb
@@ -0,0 +1,20 @@
+def get_quote(person)
+  if person == 'Yoda'
+    return 'Do. Or do not. There is no try.'
+  end
+
+  if person == 'Confucius'
+    return 'I hear and I forget. I see and I remember. I do and I understand.'
+  end
+
+  if person == 'Einstein'
+    return 'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+  end
+end
+
+puts 'Confucius says:'
+puts '"' + get_quote('Confucius') + '"'
+
+# the problem is that the function is evaluating every if statement
+# and is not returning the desired value
+# We can fix this by adding explicit returns
